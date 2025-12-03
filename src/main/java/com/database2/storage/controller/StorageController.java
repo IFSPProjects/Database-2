a1f89ae3d35c8b6e4326bfb45af3438398bfe78e
omniscientArchivist0 <reservedoracle@gmail.com>
2025-12-03 09:02:39 -0300

    Foundational state of the project. Database is finished, time to make the API logic.

---
+++ src/main/java/com/database2/storage/controller/StorageController.java
@@ -0,0 +1,10 @@
+package com.database2.storage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/storage")
+public class StorageController {
+    
+}
---
+++ src/test/java/com/database2/storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database2.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
